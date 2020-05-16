5f117e268567a7f058790040a3cc7f7fa711aa0f
meeday <59993824+meeday@users.noreply.github.com>
2020-05-16 15:10:27 +0100

    portfolio site basic structure and styling added

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $(".menu-icon").on("click", function() {
+        $("nav ul").toggleClass("showing");
+  });
+  // Scrolling Effect
+  
+  $(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+          $('nav').addClass('black');
+    }
+  
+    else {
+          $('nav').removeClass('black');
+    }
+  })
+});
