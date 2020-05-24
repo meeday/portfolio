d6c4639423a8d2f3973cf4934bfe69553cecfc9f
meeday <59993824+meeday@users.noreply.github.com>
2020-05-24 17:20:15 +0100

    Additional Comments Added

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
         $("nav ul").toggleClass("showing");
   });
   // Scrolling Effect
+//   When a scroll is detected it gives the transparent navbar a black background
   
   $(window).on("scroll", function() {
     if($(window).scrollTop()) {
